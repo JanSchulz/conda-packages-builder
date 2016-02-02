1daa060032ecc9c9c8fbc2c977a8edebb7aca65a
Jan Schulz <jasc@gmx.net>
2016-02-02 23:07:37 +0100

    New recipe for windows

--- recipes/example-package/bld.bat
+++ recipes/example-package/bld.bat
@@ -3,5 +3,5 @@ mkdir %SCRIPTS%
 echo "hello world" >  %SCRIPTS%\example.txt
 set >> %SCRIPTS%\example.txt
 
-echo "Finished" >>  %SCRIPTS%\example.txt
-
+# must be explicitly, no idea why...
+exit 0
